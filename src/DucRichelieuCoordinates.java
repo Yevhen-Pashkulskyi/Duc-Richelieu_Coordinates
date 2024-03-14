e14a273634722757b2fc8d891823d9d3d1b12c3a
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-03-15 00:12:51 +0200

    Initial project version

---
+++ src/DucRichelieuCoordinates.java
@@ -0,0 +1,10 @@
+package hillel.lesson2.src;
+
+public class DucRichelieuCoordinates {
+    public static void main(String[] args) {
+        double northernLatitude = 46.488137;
+        double EasternLongitude = 30.741146;
+        System.out.printf("Координати пам'ятника Дюк де Решельє : %.6f, %.6f ",
+                northernLatitude, EasternLongitude);
+    }
+}
